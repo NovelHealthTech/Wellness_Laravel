c77f56572162b57982f4832f58789729197ba90f
NovelHealthTech <tech@novelhealthtech.com>
2026-02-12 07:43:18 +0000

    app_blade_changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Default Title' }}</title>
+    <title>Novel Healthtech</title>
     <meta name="description" content="{{ $description ?? '' }}">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <style>
