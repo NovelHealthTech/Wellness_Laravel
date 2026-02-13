c15f23bbe48227c1b5057ee3c51440702ce83452
NovelHealthTech <tech@novelhealthtech.com>
2026-02-13 07:41:26 +0000

    new_changes

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -253,10 +253,10 @@
                                 <!--change to offline or busy as needed-->
                             </div>
                             <div class="nav-profile-text d-flex flex-column">
-                                <span class="font-weight-bold mb-2">David Grey. H</span>
+                                {{-- <span class="font-weight-bold mb-2">David Grey. H</span>
                                 <span class="text-secondary text-small">
                                     {{ auth()->user()->getRoleNames()->first() ?? 'No Role' }}
-                                </span>
+                                </span> --}}
 
                             </div>
                             <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
